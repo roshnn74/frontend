8b8a30c14948bfa2cc9c3a1dea02c2386740a8cc
ShivaRK <shivarkcodes@gmail.com>
2023-09-12 02:12:03 +0530

    contactus made responsive & functional .env added

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,51 +1,68 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import { Col, Container, Row } from 'react-bootstrap';
 
 
 const ContactUs = () => {
-  const [formStatus, setFormStatus] = React.useState('Send')
+  const [formStatus, setFormStatus] = React.useState('SEND')
+  let scriptUrl = process.env.REACT_APP_SCRIPT_URL;
+  const formRef = useRef(null);
   const onSubmit = (e) => {
     e.preventDefault()
     setFormStatus('Submitting...')
-    const { name, email, message } = e.target.elements
-    let conFom = {
-      name: name.value,
-      email: email.value,
-      message: message.value,
-    }
-    console.log(conFom)
+    fetch(scriptUrl, {
+      method: 'POST',
+      body: new FormData(formRef.current)
+    }).then(res => {
+      e.target.reset()
+      setFormStatus('Send')
+      console.log("SUCCESSFULLY SUBMITTED")
+    })
+      .catch((err) => {
+        console.log(err);
+      })
+    // const { name, email, message } = e.target.elements
+    // let conFom = {
+    //   name: name.value,
+    //   email: email.value,
+    //   message: message.value,
+    // }
+    // console.log(conFom)
   }
   return (
-      <div className="contactus">
-    <Row className="a0">
+    <div className="contactus">
+      <Row className="a0">
 
-      <Col className="a1 " lg={12} xl={4} sm={12}>
-      <h1 className='a11'>Get</h1>
-      <h3 className='a12'>in Touch</h3>
-      
-      </Col>
-      <Col className="a2 " lg={12} xl={8} sm={12}>
-        
-      <form onSubmit={onSubmit}>
-        
-        <div className="mb-3">
-          <label>NAME </label>
-          <input className="form-control" placeholder="" type="text" id="name" required />
-        </div>
-        <div className="mb-3">
-        <label>Email </label>
-          <input className="form-control" placeholder="" type="email" id="email" required />
-        </div>
-        <div className="mb-3">
-        <label>Message </label> 
-          <textarea className="form-control" placeholder="" id="message" required />
-        </div>
-        <button className="btn btn-light" type="submit">
-          {formStatus}
-        </button>
-      </form>
-    </Col>
-    </Row>
+        <Col className="a1 " lg={12} xl={4} sm={12}>
+          <div className='d-flex align-items-center justify-content-center'>
+          <span className='a10 fs-1'>✩</span>
+          <h1 className='a11'>Get</h1>
+          <span className='a10 fs-1'>✩</span>
+          </div>
+          <h3 className='a12'>in Touch</h3>
+
+        </Col>
+        <Col className="a2 " lg={12} xl={8} sm={12}>
+
+          <form ref={formRef} onSubmit={onSubmit}>
+
+            <div className="mb-3">
+              <label>NAME </label>
+              <input className="form-control" placeholder="" name='name' type="text" id="name" required />
+            </div>
+            <div className="mb-3">
+              <label>EMAIL </label>
+              <input className="form-control" placeholder="" name='email' type="email" id="email" required />
+            </div>
+            <div className="mb-3">
+              <label>MESSAGE </label>
+              <textarea className="form-control" placeholder="" name='message' id="message" required />
+            </div>
+            <button className="btn btn-light" type="submit">
+              {formStatus}
+            </button>
+          </form>
+        </Col>
+      </Row>
     </div>
   )
 }
